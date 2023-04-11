90d82bce4ca94cf50f56e968ee5a71257a2b9aa2
Devilpacks <mangyr97@mail.ru>
2023-04-11 20:11:18 +0300

    rm default

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), WalletsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
