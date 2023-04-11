798dcb2944d4fb7f252d8a78c79473d9707fd16c
Devilpacks <mangyr97@mail.ru>
2023-04-12 01:53:58 +0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { WalletsModule } from './wallets/wallets.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WalletsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    WalletsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+      logging: ["error"]
+    }),
+  ],
 })
 export class AppModule {}
