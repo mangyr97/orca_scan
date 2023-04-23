c3feceda5a22e868e011285e7ec6baa0ff61286f
Devilpacks <mangyr97@mail.ru>
2023-04-23 19:01:03 +0300

    upd test

--- src/balances/providers/test/ethereum.spec.ts
+++ src/balances/providers/test/ethereum.spec.ts
@@ -1,12 +1,14 @@
-import { EthereumProvider, EthereumOptions } from "../index";
+import { chainList } from "../config";
+import { EvmOptions } from "../interface";
+import { EvmProvider } from "../provider";
 
 describe('Ethereum provider test', () => {
-    const options: EthereumOptions = {
-        url:process.env.ETH_RPC
+    const options: EvmOptions = {
+        metadata: chainList[0]
     };
     const contract = '0x0D8775F648430679A709E98d2b0Cb6250d2887EF';
     const address = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
-    const provider = new EthereumProvider(options);
+    const provider = new EvmProvider(options);
     test('getBalanceByAddress()', async () => {
         const balance = await provider.getBalanceByAddress(address);
         expect(balance.gt(0)).toBe(true);
@@ -16,8 +18,8 @@ describe('Ethereum provider test', () => {
         expect(balance.gt(0)).toBe(true);
     });
     test('getTokensBalancesByAddress()', async () => {
-        const balance = await provider.getTokensBalancesByAddress('0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503', ['0xdAC17F958D2ee523a2206206994597C13D831ec7','0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48']);
-        // console.log(balance);
+        const balance = await provider.getTokensBalancesByAddress('0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503');
+        console.log(balance);
     });
     test('getTokenDecimals()', async () => {
         const balance = await provider.getTokenBalanceByAddress(address, contract);
